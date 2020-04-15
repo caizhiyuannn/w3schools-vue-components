c10f1d7f0379db58e0197a553525425fde66c481
caizhiyuannn <caizhiyuannn@gmail.com>
2020-04-15 18:19:38 +0800

    add examples

---
+++ src/views/accordion.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="accordion">
+    <h3>accordion examples</h3>
+    <hr />
+    <w3c-accordion title="testing">
+      <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas voluptate deleniti a? Repudiandae nesciunt eum consequuntur veniam ipsa, dignissimos explicabo!</div>
+    </w3c-accordion>
+    <w3c-accordion title="testing">
+      <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, quo vitae veniam fugit reprehenderit eveniet qui ducimus rem iusto eligendi?</div>
+    </w3c-accordion>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'accordion-example'
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/tabs.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="tabs">
+    <h3>tabs examples</h3>
+    <hr />
+    <w3c-tabs activeName="menu1" position="left">
+      <w3c-tab-panel name="menu1">Lorem ipsum dolor sit amet consectetur adipisicing elit. 1</w3c-tab-panel>
+      <w3c-tab-panel name="menu2">Lorem ipsum dolor sit amet consectetur adipisicing elit. 2</w3c-tab-panel>
+      <w3c-tab-panel name="menu3">Lorem ipsum dolor sit amet consectetur adipisicing elit. 3</w3c-tab-panel>
+    </w3c-tabs>
+    <w3c-tabs activeName="menu1" position="right">
+      <w3c-tab-panel name="menu1">Lorem ipsum dolor sit amet consectetur adipisicing elit. 1</w3c-tab-panel>
+      <w3c-tab-panel name="menu2">Lorem ipsum dolor sit amet consectetur adipisicing elit. 2</w3c-tab-panel>
+      <w3c-tab-panel name="menu3">Lorem ipsum dolor sit amet consectetur adipisicing elit. 3</w3c-tab-panel>
+    </w3c-tabs>
+    <w3c-tabs activeName="menu1" position="bottom">
+      <w3c-tab-panel name="menu1">Lorem ipsum dolor sit amet consectetur adipisicing elit. 1</w3c-tab-panel>
+      <w3c-tab-panel name="menu2">Lorem ipsum dolor sit amet consectetur adipisicing elit. 2</w3c-tab-panel>
+      <w3c-tab-panel name="menu3">Lorem ipsum dolor sit amet consectetur adipisicing elit. 3</w3c-tab-panel>
+    </w3c-tabs>
+    <w3c-tabs activeName="menu1" position="top">
+      <w3c-tab-panel name="menu1">Lorem ipsum dolor sit amet consectetur adipisicing elit. 1</w3c-tab-panel>
+      <w3c-tab-panel name="menu2">Lorem ipsum dolor sit amet consectetur adipisicing elit. 2</w3c-tab-panel>
+      <w3c-tab-panel name="menu3">Lorem ipsum dolor sit amet consectetur adipisicing elit. 3</w3c-tab-panel>
+    </w3c-tabs>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tabs-example'
+};
+</script>
+
+<style>
+</style>
