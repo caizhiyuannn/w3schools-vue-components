aa4100740bd7f157e4bf3d5e4cd160546f51d6ae
caizhiyuannn <caizhiyuannn@gmail.com>
2020-04-16 23:23:39 +0800

    support side nav

--- src/app.vue
+++ src/app.vue
@@ -4,6 +4,7 @@
     <tabs-example></tabs-example>
     <menu-example></menu-example>
     <menu-search-example />
+    <menu-side-example />
   </div>
 </template>
 
@@ -12,10 +13,12 @@ import Tabs from './views/tabs';
 import Accordion from './views/accordion';
 import Menu from './views/menu/menu.vue';
 import MenuSearch from './views/menu/menu-search';
+import MenuSide from './views/menu/menu-side';
 
 export default {
   name: 'app',
   components: {
+    [MenuSide.name]: MenuSide,
     'menu-search-example': MenuSearch,
     'menu-example': Menu,
     'tabs-example': Tabs,
---
+++ src/views/menu/menu-side.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="menu-side">
+    <h3>Side navigation example</h3>
+    <hr />
+    <div class="menu-control" @click="show = !show">
+      <span class="material-icons">menu</span>
+    </div>
+    <transition name="fade">
+      <w3c-menu class="menu" v-show="show">
+        <div class="close" @click="show = !show">
+          <span class="material-icons">close</span>
+        </div>
+        <w3c-menu-item>menu1</w3c-menu-item>
+        <w3c-menu-item>menu2</w3c-menu-item>
+        <w3c-menu-item>menu3</w3c-menu-item>
+        <w3c-menu-item>menu4</w3c-menu-item>
+      </w3c-menu>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'menu-side-example',
+  data() {
+    return {
+      show: false
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.menu {
+  display: flex;
+  flex-direction: column;
+  width: 250px;
+}
+
+.close {
+  padding: 14px 16px;
+  margin-left: auto;
+  .material-icons {
+    cursor: pointer;
+    &:hover {
+      color: #ddd;
+    }
+  }
+}
+.menu-control {
+  .material-icons {
+    cursor: pointer;
+  }
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: width 0.5s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  width: 0;
+}
+</style>
