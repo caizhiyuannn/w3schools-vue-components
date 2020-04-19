831b6a87c743ce244de205841b7017452a41153b
caizhiyuannn <caizhiyuannn@gmail.com>
2020-04-20 02:38:30 +0800

    更改组件展示逻辑

--- src/app.vue
+++ src/app.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="app">
-    <accordion-example></accordion-example>
-    <tabs-example></tabs-example>
-    <menu-example></menu-example>
-    <menu-search-example />
-    <menu-side-example />
+    <w3c-card v-for="(comp, index) in comps" :key="index">
+      <component :is="comp" />
+    </w3c-card>
   </div>
 </template>
 
@@ -14,15 +12,28 @@ import Accordion from './views/accordion';
 import Menu from './views/menu/menu.vue';
 import MenuSearch from './views/menu/menu-search';
 import MenuSide from './views/menu/menu-side';
+import Card from './components/card';
 
 export default {
   name: 'app',
   components: {
+    [Card.name]: Card,
     [MenuSide.name]: MenuSide,
     'menu-search-example': MenuSearch,
     'menu-example': Menu,
     'tabs-example': Tabs,
     'accordion-example': Accordion
+  },
+  data() {
+    return {
+      comps: [
+        Accordion.name,
+        Tabs.name,
+        Menu.name,
+        MenuSearch.name,
+        MenuSide.name
+      ]
+    };
   },
   methods: {
     action(event) {
