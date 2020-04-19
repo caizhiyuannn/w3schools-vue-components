7f0afa6e2b0c693ada47885b921dc72414f7990d
caizhiyuannn <caizhiyuannn@gmail.com>
2020-04-20 02:38:14 +0800

    添加基本卡片组件用于展示示例

---
+++ src/components/card.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="w3c-card">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'w3c-card'
+};
+</script>
+<style lang="scss" scoped>
+.w3c-card {
+  margin: 16px;
+  box-shadow: 0 4px 10px 0 rgba($color: #000000, $alpha: 0.2),
+    0 4px 20px 0 rgba($color: #000000, $alpha: 0.19);
+  padding: 8px;
+}
+</style>
