976a0ab5263693d81ef217e201fdd168496a1821
caizhiyuannn <caizhiyuannn@gmail.com>
2020-04-20 04:56:39 +0800

    add docs page

---
+++ build/webpack.page.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    app: ['./src/main.js']
+  },
+  output: {
+    path: path.resolve(__dirname, '../docs'),
+    filename: 'index.js'
+  },
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      src: path.resolve(__dirname, '../src'),
+      packages: path.resolve(__dirname, '../packages')
+    }
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          compilerOptions: {
+            preserveWhitespace: false
+          }
+        }
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass')
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(svg|otf|ttf|woff2?|eot|gif|png|jpe?g)(\?\S*)?$/,
+        loader: 'url-loader',
+        query: {
+          limit: 10000,
+          name: path.posix.join('static', '[name].[hash:7].[ext]')
+        }
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    // new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'development',
+      template: path.resolve(__dirname, '../public/index.html')
+    })
+  ]
+};
