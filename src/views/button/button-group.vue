1895711161fec9ff8beb344c1676df24cc9e4ee5
caizhiyuan <caizhiyuan@amdyes.lan>
2020-08-23 00:51:07 +0800

    button-group 示例布局调整

--- src/views/button/button-group.vue
+++ src/views/button/button-group.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="button-group">
-    <h3>button group</h3>
-    <w3c-button-group>
+    <h3>按钮组(button group)</h3>
+    <w3c-button-group class="button-group__item">
       <w3c-button type="success">button1</w3c-button>
       <w3c-button type="success">button2</w3c-button>
       <w3c-button type="success">button3</w3c-button>
+    </w3c-button-group>
+    <w3c-button-group class="button-group__item">
+      <w3c-button type="success">button1</w3c-button>
+      <w3c-button type="warning">button2</w3c-button>
+      <w3c-button type="success">button3</w3c-button>
+    </w3c-button-group>
+    <w3c-button-group class="button-group__item">
+      <w3c-button type="warning">上一页</w3c-button>
+      <w3c-button type="warning">下一页</w3c-button>
     </w3c-button-group>
   </div>
 </template>
@@ -15,4 +24,14 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.button-group__item {
+  float: left;
+  margin: 8px;
+}
+.button-group::after {
+  content: "";
+  display: block;
+  clear: both;
+}
+</style>
