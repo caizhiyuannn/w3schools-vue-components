8e1c4086cdbb7d3c99f1b6bc3b46bcd490a2c782
caizhiyuan <caizhiyuan@amdyes.lan>
2020-08-22 23:47:51 +0800

    button组件

---
+++ packages/button/index.js
@@ -0,0 +1,7 @@
+import w3cButton from "./main";
+
+w3cButton.install = function(Vue) {
+  Vue.component(w3cButton.name, w3cButton);
+};
+
+export default w3cButton;
---
+++ packages/button/main.vue
@@ -0,0 +1,72 @@
+<template>
+  <button
+    class="button"
+    :class="[
+      `button-${type}`,
+      {
+        'is-disabled': disabled,
+      },
+    ]"
+    @click="$emit('click', $event)"
+    :disabled="disabled || loading"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "w3cButton",
+  props: {
+    type: {
+      type: String,
+      default: "default",
+    },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+    disabled: Boolean,
+  },
+};
+</script>
+<style lang="scss" scoped>
+@mixin button-type($type, $bgColor, $hbgColor, $abgColor) {
+  .button-#{$type} {
+    background-color: $bgColor;
+    &:hover {
+      background-color: $hbgColor;
+    }
+    &:active {
+      background-color: $abgColor;
+    }
+  }
+}
+
+.button {
+  border: none;
+  color: white;
+  padding: 12px 16px;
+  cursor: pointer;
+  outline: none;
+}
+
+@include button-type(success, #4caf50, #46a049, #179b19);
+@include button-type(info, #2196f3, #0b7dda, #0860a8);
+@include button-type(warning, #ff9800, #e68a00, #ad6902);
+@include button-type(danger, #f44336, #da190b, #a12a21);
+@include button-type(default, #e7e7e7, #ddd, #cac7c7);
+
+.button-default {
+  color: black;
+}
+
+.is-disabled {
+  cursor: not-allowed;
+  &,
+  &:hover,
+  &:focus {
+    background-color: #cac7c7;
+  }
+}
+</style>
